d9392a7d3dfea294fb302970ba40b6c4c70daee9
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-01-25 15:36:45 -0300

    operação-de-instanciaçao

--- src/main/java/applicationwithmongoDB/ApplicationWithMongoDbApplication.java
+++ src/main/java/applicationwithmongoDB/ApplicationWithMongoDbApplication.java
@@ -1,13 +1,33 @@
 package applicationwithmongoDB;
 
+import applicationwithmongoDB.entity.User;
+import applicationwithmongoDB.repositoryMongoBD.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ApplicationWithMongoDbApplication {
+public class ApplicationWithMongoDbApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationWithMongoDbApplication.class, args);
 	}
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
 }
