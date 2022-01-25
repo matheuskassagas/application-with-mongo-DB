b45c0328c5dd1da739e225e4a21a25efb4f404c5
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-01-25 11:31:27 -0300

    project-created

---
+++ src/main/java/applicationwithmongoDB/ApplicationWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package applicationwithmongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationWithMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationWithMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/applicationwithmongoDB/ApplicationWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package applicationwithmongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationWithMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
