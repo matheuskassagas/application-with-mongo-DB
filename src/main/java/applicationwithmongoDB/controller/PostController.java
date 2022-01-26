0f8536ca6e33c13025371ae468765351ab4cf8e1
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-01-26 18:37:50 -0300

    consulta-com-varios-criterios

--- src/main/java/applicationwithmongoDB/controller/PostController.java
+++ src/main/java/applicationwithmongoDB/controller/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,16 +24,25 @@ public class PostController {
     private PostService postService;
 
 
-    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
-    public ResponseEntity<?> findById(@PathVariable String id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
     }
 
-    @RequestMapping(value = "/titlesearch",method = RequestMethod.GET)
-    public ResponseEntity<?> findByTitle (@RequestParam(value = "text", defaultValue = "") String text){
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<?> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text); //decodifica o texto no formato necessario para url
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<?> fullsearch(@RequestParam(value = "text", defaultValue = "") String text, @RequestParam(value = "minDate", defaultValue = "") String minDate, @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.converteDate(minDate, new Date(0L));
+        Date max = URL.converteDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/applicationwithmongoDB/controller/util/URL.java
+++ src/main/java/applicationwithmongoDB/controller/util/URL.java
@@ -2,6 +2,10 @@ package applicationwithmongoDB.controller.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 // classe para formatar a string da url para o padrao de busca
 public class URL {
@@ -11,6 +15,16 @@ public class URL {
             return URLDecoder.decode(text, "UTF-8");
         }catch (UnsupportedEncodingException e){
             return "";
+        }
+    }
+
+    public static Date converteDate(String textDate, Date defaultDate){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try{
+            return sdf.parse(textDate);
+        }catch (ParseException e){
+            return defaultDate;
         }
     }
 }
