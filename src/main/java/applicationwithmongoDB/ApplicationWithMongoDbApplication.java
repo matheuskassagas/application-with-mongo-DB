eb51af02a9b6e556c814112af5a9e2aef310485f
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-01-26 16:34:22 -0300

    projetando dados com dto

--- src/main/java/applicationwithmongoDB/ApplicationWithMongoDbApplication.java
+++ src/main/java/applicationwithmongoDB/ApplicationWithMongoDbApplication.java
@@ -1,5 +1,6 @@
 package applicationwithmongoDB;
 
+import applicationwithmongoDB.DTO.AuthorDTO;
 import applicationwithmongoDB.repositoryMongoBD.PostRepository;
 import applicationwithmongoDB.repositoryMongoBD.entity.Post;
 import applicationwithmongoDB.repositoryMongoBD.entity.User;
@@ -38,10 +39,11 @@ public class ApplicationWithMongoDbApplication implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21;03;2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23;03;2018"), "Bom Dia", "Acordei Feliz Hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21;03;2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23;03;2018"), "Bom Dia", "Acordei Feliz Hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
--- src/main/java/applicationwithmongoDB/repositoryMongoBD/entity/Post.java
+++ src/main/java/applicationwithmongoDB/repositoryMongoBD/entity/Post.java
@@ -1,25 +1,28 @@
 package applicationwithmongoDB.repositoryMongoBD.entity;
 
+import applicationwithmongoDB.DTO.AuthorDTO;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
 import java.security.PrivateKey;
 import java.util.Date;
 import java.util.Objects;
 
 @Document
-public class Post {
+public class Post implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String id;
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -59,11 +62,11 @@ public class Post {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
